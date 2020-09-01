41a19461c80a4f41b76448ae9ade92006a90c210
Carlos García <67180099+CarlosG4rc@users.noreply.github.com>
2020-09-01 12:07:33 -0500

    Add files via upload

---
+++ Curso Python/Funciones/Funciones.py
@@ -0,0 +1,18 @@
+def dibujar_tabla_del_5():
+    for i in range(11):
+        print("5 *",i,"=",i*5)
+dibujar_tabla_del_5()
+
+
+def test():
+    print (m)
+m=10 #Variable debe estar declarada antes de ejecutarla, no declararla.
+test()
+
+def change():
+    o=5
+    print(o)
+change()
+o=10
+change()
+print(o)
