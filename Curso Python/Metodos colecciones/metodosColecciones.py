c31e895d81959f277619d7f6a58f7ab2fc179147
Carlos García <67180099+CarlosG4rc@users.noreply.github.com>
2020-09-14 23:23:51 -0500

    Add files via upload

---
+++ Curso Python/Metodos colecciones/metodosColecciones.py
@@ -0,0 +1,13 @@
+c = "Hola mundo"
+print(c.upper())
+print(c.lower())
+print(c.capitalize())
+print(c.title())
+print(c.count('o'))
+print(c.find('mundo'))
+print(c.rfind('o'))
+
+print(c.split()[-1])
+print("Hola, mundo, otro".split(','))
+print(",".join("Hola mundo"))
+
